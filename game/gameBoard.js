afc879d032620cb1eaa30622c721b2bebc5fc8ee
Kim <fletn3000@hotmail.com>
2016-12-18 18:55:14 +0100

    Added some button interaction

--- game/gameBoard.js
+++ game/gameBoard.js
@@ -6,19 +6,34 @@ var MovingDirection = {
     IDLE: 4
 };
 
+var State = {
+    PAUSE: 0,
+    PLAY: 1
+};
+
 var Game = function (canvasId, context2d) {
     var canvas = document.getElementById(canvasId);
     var context = canvas.getContext(context2d);
     this.textColor = "#fff";
     this.player = new Player(90, 30);
     this.playerDirection = {};
-
+    this.gameState = State.PAUSE;
 
     this.init = function () {
         context.fillStyle = "#000";
         context.fillRect(0, 0, canvas.width, canvas.height);
         this.playerDirection = MovingDirection.IDLE;
-        this.gameLoop();
+
+    };
+
+    this.startGame = function () {
+        if (this.gameState === State.PAUSE) {
+            this.drawText("Shooter game", "30px verdana", canvas.width / 3, canvas.height / 4);
+            this.drawText("Press 'n' to start new game", "20px verdana", canvas.width / 3.5, canvas.height / 3);
+            setTimeout(this.startGame.bind(this), 1000 / 2);
+        } else {
+            this.gameLoop();
+        }
     };
 
     this.update = function () {
@@ -29,17 +44,7 @@ var Game = function (canvasId, context2d) {
     this.gameLoop = function () {
         this.update();
         this.draw();
-        setTimeout(this.gameLoop, 1000 / 2);
-    };
-
-    this.startGame = function () {
-        context.fillStyle = this.textColor;
-        this.drawText("Shooter game", "30px verdana", canvas.width / 3, canvas.height / 4)
-        this.drawText("Press 'n' to start new game", "20px verdana", canvas.width / 3.5, canvas.height / 3)
-    };
-
-    this.draw = function () {
-        this.drawPlayer();
+        setTimeout(this.gameLoop.bind(this), 1000 / 2);
     };
 
     this.calculatePlayerPos = function () {
@@ -53,27 +58,42 @@ var Game = function (canvasId, context2d) {
         }
     };
 
+    this.draw = function () {
+        this.resetCanvas();
+        this.drawPlayer();
+    };
+
+    this.resetCanvas = function () {
+        context.fillStyle = "#000";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+    };
+
     this.drawPlayer = function () {
         context.fillStyle = "#FFF";
         context.fillRect(canvas.width / 2 - this.player.getWidth() / 2, canvas.height - (this.player.getHeight() * 2), this.player.getWidth(), this.player.getHeight());
     };
 
     this.drawText = function (text, font, width, height) {
+        context.fillStyle = this.textColor;
         context.font = font;
         context.fillText(text, width, height);
     };
 
+    var self = this;
     document.addEventListener('keydown', function (event) {
         switch (event.keyCode) {
             case 37:
-                this.playerDirection = MovingDirection.LEFT;
+                self.playerDirection = MovingDirection.LEFT;
                 //checkPlayerMovement();
                 //draw();
                 break;
             case 39:
-                this.playerDirection = MovingDirection.RIGHT;
+                self.playerDirection = MovingDirection.RIGHT;
                 //checkPlayerMovement();
                 //draw();
+                break;
+            case 78:
+                self.gameState = State.PLAY;
                 break;
         }
     });
