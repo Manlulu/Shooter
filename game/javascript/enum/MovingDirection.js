50647007da58f47344483e5c8a0acf283054e32c
Kim <fletn3000@hotmail.com>
2016-12-20 22:12:00 +0100

    Refactor. Moved files to folders

---
+++ game/javascript/enum/MovingDirection.js
@@ -0,0 +1,7 @@
+var MovingDirection = {
+    LEFT: 0,
+    UP: 1,
+    RIGHT: 2,
+    DOWN: 3,
+    IDLE: 4
+};
--- game/javascript/player/PlayerMovement.js
+++ game/javascript/player/PlayerMovement.js
@@ -30,11 +30,3 @@ PlayerMovement = {
         }
     }
 };
-
-var MovingDirection = {
-    LEFT: 0,
-    UP: 1,
-    RIGHT: 2,
-    DOWN: 3,
-    IDLE: 4
-};
