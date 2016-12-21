30ab371e733691d0bbc9519e68787fc5f4e514b5
Kim <fletn3000@hotmail.com>
2016-12-21 22:29:41 +0100

    Auto shoot fix

--- game/javascript/gameBoard/gameBoard.js
+++ game/javascript/gameBoard/gameBoard.js
@@ -74,10 +74,14 @@ var Game = function () {
     };
 
     var updatePlayerFire = function () {
+        console.log(playerState );
+        console.log(player.getAutoFireReady());
+        console.log("Timer: " + autoFireTimer);
         if(playerState == PlayerState.FIRE && player.getAutoFireReady()){
             player.fire();
             audioPlayerShoot.play();
             player.setAutoFireReady(false);
+            resetAutoFireTimer();
         }
 
         if(autoFireTimer >= 50){
